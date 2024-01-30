c6cf6cd42cf6f32514517d14c4faf127da20cedc
nemalex <alex.nemeth_contract@playableworlds.com>
2024-01-30 12:26:10 +0100

    feat: basic layout

---
+++ main.js
@@ -0,0 +1,41 @@
+import * as monaco from 'monaco-editor';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import jsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+self.MonacoEnvironment = {
+  getWorker: function (workerId, label) {
+    switch (label) {
+      case 'json':
+        return jsonWorker();
+      case 'css':
+      case 'scss':
+      case 'less':
+        return cssWorker();
+      case 'html':
+      case 'handlebars':
+      case 'razor':
+        return htmlWorker();
+      case 'typescript':
+      case 'javascript':
+        return jsWorker();
+      default:
+        return editorWorker();
+    }
+  }
+};
+
+let editor = monaco.editor.create(document.getElementById('editor'), {
+  value: "function hello() {\n\talert('Hello world!');\n}",
+  language: 'javascript',
+  autoLayout: true,
+});
+
+function submit() {
+  // alert(editor.getValue());
+  document.getElementById("teView").innerHTML = editor.getValue();
+}
+
+document.getElementById("submitButton").onclick = submit;
