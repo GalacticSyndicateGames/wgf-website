794424e0351ee4bf1a6f5fb49d0d1e5289c19f1a
nemalex <alex.nemeth_contract@playableworlds.com>
2024-01-30 17:08:42 +0100

    chore: formatting

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-import * as monaco from 'monaco-editor';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import jsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import * as monaco from "monaco-editor";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import jsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 
 self.MonacoEnvironment = {
   getWorker: function (workerId, label) {
     switch (label) {
-      case 'json':
+      case "json":
         return jsonWorker();
-      case 'css':
-      case 'scss':
-      case 'less':
+      case "css":
+      case "scss":
+      case "less":
         return cssWorker();
-      case 'html':
-      case 'handlebars':
-      case 'razor':
+      case "html":
+      case "handlebars":
+      case "razor":
         return htmlWorker();
-      case 'typescript':
-      case 'javascript':
+      case "typescript":
+      case "javascript":
         return jsWorker();
       default:
         return editorWorker();
     }
-  }
+  },
 };
 
-let editor = monaco.editor.create(document.getElementById('editor'), {
+let editor = monaco.editor.create(document.getElementById("editor"), {
   value: "function hello() {\n\talert('Hello world!');\n}",
-  language: 'javascript',
+  language: "javascript",
   autoLayout: true,
 });
 
